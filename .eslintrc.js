28159a26229bec54ed6b5e89e36bec939bf5c28e
Jesse Anderson <jesse.anderson@sideinc.com>
2022-01-03 10:02:45 -0500

    feat: add lib foundational assets

---
+++ .eslintrc.js
@@ -0,0 +1,59 @@
+module.exports = {
+  extends: [
+    '@side/base',
+    '@side/prettier',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:jsdoc/recommended',
+  ],
+  parser: '@typescript-eslint/parser',
+  // TODO: [PLAT-827] Create a lint-config configuration for Typescript projects
+  settings: {
+    'import/extensions': ['.js', '.mjs', '.jsx', '.ts', '.tsx'],
+    'import/resolver': {
+      node: {
+        extensions: ['.mjs', '.js', '.json', '.jsx', '.ts', '.tsx'],
+      },
+    },
+  },
+  rules: {
+    'no-console': 0,
+    'jsdoc/require-returns-type': 0,
+    'jsdoc/require-param-type': 0,
+    'no-shadow': 'off',
+    '@typescript-eslint/no-shadow': 'error',
+
+    // TODO: [PLAT-827] Create a lint-config configuration for Typescript projects
+
+    // Ensure consistent use of file extension within the import path
+    // https://github.com/benmosher/eslint-plugin-import/blob/master/docs/rules/extensions.md
+    'import/extensions': [
+      'error',
+      'ignorePackages',
+      {
+        js: 'never',
+        mjs: 'never',
+        jsx: 'never',
+        ts: 'never',
+        tsx: 'never',
+      },
+    ],
+  },
+  overrides: [
+    {
+      files: ['./jest.setup.js'],
+      extends: ['@side/jest'],
+    },
+    {
+      files: ['**/__tests__/**/*.[jt]s?(x)', '**/?(*.)+(spec|test).[jt]s?(x)'],
+      rules: {
+        'jsdoc/require-jsdoc': 0,
+      },
+    },
+    {
+      files: ['**/*.ts'],
+      rules: {
+        'import/prefer-default-export': 0,
+      },
+    },
+  ],
+};
