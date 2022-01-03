2257363a990ece09d6756dd6ee74ba5e39a7bde0
Jesse Anderson <jesse.anderson@sideinc.com>
2022-01-03 15:19:02 -0500

    tests: adding tests for main entrypoint

--- src/__tests__/index.ts
+++ src/__tests__/index.ts
@@ -1,7 +1,47 @@
 import { fortifyHeaders } from '..'
 
 describe('fortify-core entrypoint tests', function fortifyCoreEntrypointTests() {
-  it('returns defaults to caller when no properties exist on incoming object', function test() {
-    return fortifyHeaders({});
+  it('returns defaults for Content-Security-Policy when nothing is specified', function test() {
+    const fortifiedHeaders = fortifyHeaders({
+      contentSecurityPolicy: {}
+    });
+
+    expect(fortifiedHeaders).toEqual({
+      'Content-Security-Policy': ''
+    });
+  });
+
+  it('returns defaults for Cross-Origin-Embedder-Policy when nothing is specified', function test() {
+
+  });
+
+  it('returns defaults for Cross-Origin-Resource-Policy when nothing is specified', function test() {
+  });
+
+  it('returns defaults for Expect-CT when nothing is specified', function test() {
+  });
+
+  it('returns defaults for Origin-Agent-Cluster when nothing is specified', function test() {
+  });
+
+  it('returns defaults for Referrer-Policy when nothing is specified', function test() {
+  });
+
+  it('returns defaults for Strict-Transport-Security when nothing is specified', function test() {
+  });
+
+  it('returns defaults for X-Content-Type-Options when nothing is specified', function test() {
+  });
+
+  it('returns defaults for X-DNS-Prefetch-Control when nothing is specified', function test() {
+  });
+
+  it('returns defaults for X-Download-Options when nothing is specified', function test() {
+  });
+
+  it('returns defaults for X-Frame-Options when nothing is specified', function test() {
+  });
+
+  it('returns defaults for X-Permitted-Cross-Domain-Policies when nothing is specified', function test() {
   });
 });
