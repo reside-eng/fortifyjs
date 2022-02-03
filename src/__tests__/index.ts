7bff7a1a1d35dd5959dd78d485ee3bd949668505
Jesse Anderson <jesse.anderson@sideinc.com>
2022-02-03 10:11:52 -0500

    fix: implicit default enforcement

--- src/__tests__/index.ts
+++ src/__tests__/index.ts
@@ -23,6 +23,27 @@ describe('fortify-core entrypoint tests', () => {
       });
     });
 
+    it('implicitly defaults with no parameters', () => {
+      const fortifiedHeaders = fortifyHeaders();
+
+      expect(fortifiedHeaders).toEqual({
+        'Content-Security-Policy':
+          "default-src 'self'; base-uri 'self'; font-src 'self' https: data:; frame-ancestors 'self'; img-src 'self' data:; object-src 'none'; script-src 'self'; script-src-attr 'none'; style-src 'self' https: 'unsafe-inline'; upgrade-insecure-requests",
+        'Cross-Origin-Embedder-Policy': 'require-corp',
+        'Cross-Origin-Opener-Policy': 'same-origin',
+        'Cross-Origin-Resource-Policy': 'same-origin',
+        'Expect-Ct': 'max-age=0',
+        'Origin-Agent-Cluster': '?1',
+        'Referrer-Policy': 'no-referrer',
+        'Strict-Transport-Security': 'max-age=15552000',
+        'X-Content-Type-Options': 'nosniff',
+        'X-Dns-Prefetch-Control': 'off',
+        'X-Download-Options': 'noopen',
+        'X-Frame-Options': 'SAMEORIGIN',
+        'X-Permitted-Cross-Domain-Policies': 'none',
+      });
+    });
+
     it('customizes some properties and defaults others', () => {
       const fortifiedHeaders = fortifyHeaders({
         contentSecurityPolicy: {},
