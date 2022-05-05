b0f69175f9c86ae2c4f099300a907f4c4c80ffe8
Jesse Anderson <jesse.anderson@sideinc.com>
2022-05-05 17:57:22 -0400

    fix: update x-content-type-options types

--- src/headers/x-content-type-options/types.ts
+++ src/headers/x-content-type-options/types.ts
@@ -4,11 +4,9 @@ import { FortifyHeader } from '../types';
  * Represents the user-specified header configuration for X-Content-Type-Options
  * see: https://developer.mozilla.org/en-US/docs/Web/HTTP/Headers/X-Content-Type-Options
  */
-export type XContentTypeOptions =
-  | FortifyHeader
-  | {
-      /**
-       * Blocks a request if the request destination is of type style and the MIME type is not text/css, or of type script and the MIME type is not a JavaScript MIME type
-       */
-      nosniff?: boolean;
-    };
+export type XContentTypeOptions = FortifyHeader & {
+  /**
+   * Blocks a request if the request destination is of type style and the MIME type is not text/css, or of type script and the MIME type is not a JavaScript MIME type
+   */
+  nosniff?: boolean;
+};
